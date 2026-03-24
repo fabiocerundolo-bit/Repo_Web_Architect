70000ba4b32e82275fd2413e78a7d6d7b53083c8
= <=>
2026-03-24 12:25:03 +0100

    update

--- JAVASCRIPT/Esercitazioni/esercitazione2/js/script0.js
+++ JAVASCRIPT/Esercitazioni/esercitazione2/js/script0.js
@@ -1,15 +1,45 @@
-const PAROLA = "COMPUTER";
-
+const btnTest = document.getElementById("btnTest");
 const risultato = document.getElementById("risultato");
+const elLettera = document.getElementById("lettera");
 
-function caricaParola(){
+const PAROLASEGRETA = "CASA";
+let parolaMostrata = [];
+
+function inizializzaGioco(){
     let parolaGhost = "";
 
-    PAROLA.split("").forEach(lettera => {
+    PAROLASEGRETA.split("").forEach(lettera => {
         parolaGhost += "_ ";
+        parolaMostrata.push("_");
     })
-
+    // parolaGhost = PAROLASEGRETA.split("").fill("_").join(" ");
     risultato.innerHTML += `<h2 class='txtCenter'> ${parolaGhost} </h2>`
 }
 
-document.addEventListener("DOMContentLoaded", caricaParola)
+/**
+ * 
+ * @param {String} lettera 
+ */
+function controllaLettera(lettera){
+    lettera = lettera.toUpperCase();
+
+    risultato.innerHTML = "";
+
+    for(let i = 0; i < PAROLASEGRETA.length; i++){
+        if(PAROLASEGRETA[i] === lettera && parolaMostrata[i] != lettera){
+            parolaMostrata[i] = lettera;
+            parolaMostrata.forEach(lettera => {
+                risultato.innerHTML += `${lettera} `;
+            });
+        }
+    }
+}
+
+btnTest.addEventListener("click", function(){
+    controllaLettera(elLettera.value);
+})
+
+
+
+
+document.addEventListener("DOMContentLoaded", inizializzaGioco)
