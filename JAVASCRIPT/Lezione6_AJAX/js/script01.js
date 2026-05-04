6539568a10801bc23b4cca40bfeda6f399713e6e
= <=>
2026-05-04 13:00:42 +0200

    update

---
+++ JAVASCRIPT/Lezione6_AJAX/js/script01.js
@@ -0,0 +1,28 @@
+const demo = document.querySelector("#demo");
+
+const URL = "https://jsonplaceholder.typicode.com/users?q='Roma-Torino'o='07:00'"; //endopoint
+
+let utenti = [];
+
+fetch(URL)
+.then(data =>{
+    console.log("Qui sto aspettando i dati dal server"); //Questo è il punto in cui metto lo spinner
+    console.log(data.status);
+    
+    return data.json();
+})
+.then(response =>{
+    console.log(response);
+    utenti = response;
+    console.log("I miei utenti sono: ", utenti);
+})
+.finally( ()=>{
+
+    console.log("Fetch Terminata");
+}
+)
+
+
+console.log("I miei utenti sono: ", utenti); //Qui sono fuori dalla fetch, quindi sono in ASINCRONO, non vedo i dati
+
+
